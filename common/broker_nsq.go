b6288dbd9afdbc05b6f9d2321c88a272a647220f
Étienne Lafarge <etienne.lafarge@gmail.com>
2017-06-21 00:49:26 +0200

    [nsq] Make NSQ logger configurable

--- common/broker_nsq.go
+++ common/broker_nsq.go
@@ -1,23 +1,23 @@
 /*
  * Copyright Morpheo Org. 2017
- * 
+ *
  * contact@morpheo.co
- * 
+ *
  * This software is part of the Morpheo project, an open-source machine
  * learning platform.
- * 
+ *
  * This software is governed by the CeCILL license, compatible with the
  * GNU GPL, under French law and abiding by the rules of distribution of
  * free software. You can  use, modify and/ or redistribute the software
  * under the terms of the CeCILL license as circulated by CEA, CNRS and
  * INRIA at the following URL "http://www.cecill.info".
- * 
+ *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
  * liability.
- * 
+ *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
  * software by the user in light of its specific status of free software,
@@ -28,7 +28,7 @@
  * requirements in conditions enabling the security of their systems and/or
  * data to be ensured and,  more generally, to use and operate it in the
  * same conditions as regards security.
- * 
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
@@ -92,15 +92,17 @@ type ConsumerNSQ struct {
 	LookupUrls           []string
 	QueuePollingInterval time.Duration
 	Channel              string
+	Logger               *log.Logger
 }
 
 // NewNSQConsumer instantiates ConsumerNSQ for the provided channel, using provided nsqlookupd URLs
-func NewNSQConsumer(lookupUrls []string, channel string, queuePollingInterval time.Duration) (c *ConsumerNSQ) {
+func NewNSQConsumer(lookupUrls []string, channel string, queuePollingInterval time.Duration, logger *log.Logger) (c *ConsumerNSQ) {
 	return &ConsumerNSQ{
 		LookupUrls:           lookupUrls,
 		Channel:              channel,
 		QueuePollingInterval: queuePollingInterval,
 		NsqConsumer:          map[string]*nsq.Consumer{},
+		Logger:               logger,
 	}
 }
 
@@ -143,6 +145,7 @@ func (c *ConsumerNSQ) AddHandler(topic string, handler Handler, concurrency int,
 	if err != nil {
 		return fmt.Errorf("Error creating NSQ Consumer for topic %s: %s", topic, err)
 	}
+	consumer.SetLogger(c.Logger, nsq.LogLevelWarning)
 	consumer.AddConcurrentHandlers(newHandlerWrapper(handler), concurrency)
 	c.NsqConsumer[topic] = consumer
 
